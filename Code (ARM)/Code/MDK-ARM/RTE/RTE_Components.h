a6b5604875b9fe10dcddb6e8b0553fdc480b1a50
Amin Razzaghi <amin.razzaghi97@gmail.com>
2021-12-28 15:34:03 +0330

    the whole project

---
+++ Code (ARM)/Code/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'AMIRGOSSEIN' 
+ * Target:  'AMIRGOSSEIN' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
